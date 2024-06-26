e625766a4a772daff58edbb63a72c528c4699b56
Slimslim <selim.gawad@gmail.com>
2024-06-25 18:57:42 -0700

    Initial Commit

---
+++ WorkoutApp/WorkoutAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutAppApp.swift
+//  WorkoutApp
+//
+//  Created by Sélim Gawad on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
