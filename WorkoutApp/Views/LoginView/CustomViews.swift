bcbac1c39741f4fd6e85e2a5a91c985db8dcfcce
Slimslim <selim.gawad@gmail.com>
2024-07-20 21:02:56 +0200

    Adding Login screen, not fully working yet

---
+++ WorkoutApp/Views/LoginView/CustomViews.swift
@@ -0,0 +1,18 @@
+//
+//  CustomViews.swift
+//  WorkoutApp
+//
+//  Created by Sélim Gawad on 7/16/24.
+//
+
+import SwiftUI
+
+struct CustomViews: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomViews()
+}
