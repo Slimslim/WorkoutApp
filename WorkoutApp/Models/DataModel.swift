b8ebdc7618edfa3f00f868a30a694f6f3b197cdd
Slimslim <selim.gawad@gmail.com>
2024-08-19 18:21:45 -0700

    New Branch to change the database upload. Change data model to split workout information and motion data into 2 difrent DB

--- WorkoutApp/Models/DataModel.swift
+++ WorkoutApp/Models/DataModel.swift
@@ -5,22 +5,30 @@
 //  Created by Sélim Gawad on 6/25/24.
 //
 
-
 import Foundation
 import CoreMotion
 import RealmSwift
 
-class Workout: Object, ObjectKeyIdentifiable{
-    @Persisted(primaryKey: true) var _id:ObjectId
+/// The `Workout` class represents the main workout entity.
+/// It contains general information about the workout session, such as the user's name,
+/// workout details, and a reference to the time-based batched data.
+class Workout: Object, ObjectKeyIdentifiable {
+    @Persisted(primaryKey: true) var _id: ObjectId
     @Persisted var created = Date()
     @Persisted var username: String
     @Persisted var notes: String?
     @Persisted var isDataGood: Bool
-    @Persisted var info : WorkoutInfo?
-    @Persisted var data : WorkoutData?
+    @Persisted var info: WorkoutInfo?
+    // Removed direct data embedding. Data is now stored in separate batch documents.
     
-    
-    convenience init(created: Date? = nil, username: String, notes: String? = nil, isDataGood: Bool = false, info: WorkoutInfo? = nil, data: WorkoutData? = nil){
+    /// Initializes a new `Workout` object.
+    /// - Parameters:
+    ///   - created: The date and time when the workout was created. Defaults to the current date and time.
+    ///   - username: The name of the user who performed the workout.
+    ///   - notes: Any additional notes related to the workout.
+    ///   - isDataGood: A boolean indicating if the collected data is good for analysis.
+    ///   - info: Detailed information about the workout.
+    convenience init(created: Date? = nil, username: String, notes: String? = nil, isDataGood: Bool = false, info: WorkoutInfo? = nil) {
         self.init()
         if let created = created {
             self.created = created
@@ -29,45 +37,93 @@ class Workout: Object, ObjectKeyIdentifiable{
         self.notes = notes
         self.isDataGood = isDataGood
         self.info = info
-        self.data = data
     }
-    
 }
 
+/// The `WorkoutInfo` class holds details about the workout, such as the movement performed,
+/// the number of rounds and reps, and the weight used. It also includes metadata like the start
+/// and end times of the workout and the total number of data batches.
 class WorkoutInfo: EmbeddedObject {
-    
     @Persisted var movement: String
     @Persisted var rounds: Int
     @Persisted var reps: Int
     @Persisted var weight: Double?
-    
-    convenience init( movement: String, rounds: Int, reps: Int, weight: Double? = nil) {
+    @Persisted var startTime: Date?  // Start time of the workout
+    @Persisted var endTime: Date?  // End time of the workout
+    @Persisted var numberOfBatches: Int?  // Total number of data batches
+
+    /// Initializes a new `WorkoutInfo` object.
+    /// - Parameters:
+    ///   - movement: The type of movement performed during the workout.
+    ///   - rounds: The number of rounds completed.
+    ///   - reps: The number of repetitions per round.
+    ///   - weight: The weight used during the workout, if applicable.
+    ///   - startTime: The start time of the workout.
+    ///   - endTime: The end time of the workout.
+    ///   - numberOfBatches: The total number of data batches generated during the workout.
+    convenience init(movement: String, rounds: Int, reps: Int, weight: Double? = nil, startTime: Date? = nil, endTime: Date? = nil, numberOfBatches: Int? = nil) {
         self.init()
         self.movement = movement
         self.rounds = rounds
         self.reps = reps
         self.weight = weight
+        self.startTime = startTime
+        self.endTime = endTime
+        self.numberOfBatches = numberOfBatches
     }
 }
 
-class WorkoutData: EmbeddedObject {
-    @Persisted var accelerometerSnapshots = List <AccelerometerData>()
-    @Persisted var gyroscopeSnapshots = List <GyroscopeData>()
-    
-    convenience init(accelerometerSnapshots: [AccelerometerData], gyroscopeSnapshots: [GyroscopeData]) {
+/// The `WorkoutDataBatch` class represents a single time-based batch of motion data.
+/// It includes the start and end times of the batch, the capture rates for the accelerometer and gyroscope,
+/// and lists of the accelerometer and gyroscope data snapshots.
+class WorkoutDataBatch: Object, ObjectKeyIdentifiable {
+    @Persisted(primaryKey: true) var _id: ObjectId
+    @Persisted var workoutId: ObjectId  // Link back to the Workout
+    @Persisted var batchNumber: Int  // To identify the order of the batch
+    @Persisted var startTime: Date
+    @Persisted var endTime: Date
+    @Persisted var accelerometerRate: Int  // Capture rate of accelerometer in Hz
+    @Persisted var gyroscopeRate: Int  // Capture rate of gyroscope in Hz
+    @Persisted var accelerometerSnapshots = List<AccelerometerData>()
+    @Persisted var gyroscopeSnapshots = List<GyroscopeData>()
+
+    /// Initializes a new `WorkoutDataBatch` object.
+    /// - Parameters:
+    ///   - workoutId: The identifier of the workout to which this batch belongs.
+    ///   - batchNumber: The sequential number of this batch.
+    ///   - startTime: The start time of this batch.
+    ///   - endTime: The end time of this batch.
+    ///   - accelerometerRate: The capture rate of the accelerometer in Hz.
+    ///   - gyroscopeRate: The capture rate of the gyroscope in Hz.
+    ///   - accelerometerSnapshots: The list of accelerometer data snapshots in this batch.
+    ///   - gyroscopeSnapshots: The list of gyroscope data snapshots in this batch.
+    convenience init(workoutId: ObjectId, batchNumber: Int, startTime: Date, endTime: Date, accelerometerRate: Int, gyroscopeRate: Int, accelerometerSnapshots: [AccelerometerData], gyroscopeSnapshots: [GyroscopeData]) {
         self.init()
+        self.workoutId = workoutId
+        self.batchNumber = batchNumber
+        self.startTime = startTime
+        self.endTime = endTime
+        self.accelerometerRate = accelerometerRate
+        self.gyroscopeRate = gyroscopeRate
         self.accelerometerSnapshots.append(objectsIn: accelerometerSnapshots)
         self.gyroscopeSnapshots.append(objectsIn: gyroscopeSnapshots)
     }
-    
 }
 
+/// The `AccelerometerData` class represents a single snapshot of accelerometer data.
+/// Each snapshot contains the timestamp and the acceleration values along the X, Y, and Z axes.
 class AccelerometerData: EmbeddedObject {
     @Persisted var timestamp: TimeInterval
     @Persisted var accelerationX: Double
     @Persisted var accelerationY: Double
     @Persisted var accelerationZ: Double
 
+    /// Initializes a new `AccelerometerData` object.
+    /// - Parameters:
+    ///   - timestamp: The time at which the data was captured.
+    ///   - accelerationX: The acceleration along the X axis.
+    ///   - accelerationY: The acceleration along the Y axis.
+    ///   - accelerationZ: The acceleration along the Z axis.
     convenience init(timestamp: TimeInterval, accelerationX: Double, accelerationY: Double, accelerationZ: Double) {
         self.init()
         self.timestamp = timestamp
@@ -77,12 +133,20 @@ class AccelerometerData: EmbeddedObject {
     }
 }
 
+/// The `GyroscopeData` class represents a single snapshot of gyroscope data.
+/// Each snapshot contains the timestamp and the rotation values along the X, Y, and Z axes.
 class GyroscopeData: EmbeddedObject {
     @Persisted var timestamp: TimeInterval
     @Persisted var rotationX: Double
     @Persisted var rotationY: Double
     @Persisted var rotationZ: Double
 
+    /// Initializes a new `GyroscopeData` object.
+    /// - Parameters:
+    ///   - timestamp: The time at which the data was captured.
+    ///   - rotationX: The rotation rate around the X axis.
+    ///   - rotationY: The rotation rate around the Y axis.
+    ///   - rotationZ: The rotation rate around the Z axis.
     convenience init(timestamp: TimeInterval, rotationX: Double, rotationY: Double, rotationZ: Double) {
         self.init()
         self.timestamp = timestamp
@@ -92,34 +156,39 @@ class GyroscopeData: EmbeddedObject {
     }
 }
 
-
-
-//@Model
-//class WorkoutDataChunk {
-//    var id: UUID
-//    var info: WorkoutInfoData
-//    var data: WorkoutMotionData?
-//
-//    init(id: UUID, info: WorkoutInfoData, data: WorkoutMotionData? = nil) {
-//        self.id = id
+//class Workout: Object, ObjectKeyIdentifiable{
+//    @Persisted(primaryKey: true) var _id:ObjectId
+//    @Persisted var created = Date()
+//    @Persisted var username: String
+//    @Persisted var notes: String?
+//    @Persisted var isDataGood: Bool
+//    @Persisted var info : WorkoutInfo?
+//    @Persisted var data : WorkoutData?
+//    
+//    
+//    convenience init(created: Date? = nil, username: String, notes: String? = nil, isDataGood: Bool = false, info: WorkoutInfo? = nil, data: WorkoutData? = nil){
+//        self.init()
+//        if let created = created {
+//            self.created = created
+//        }
+//        self.username = username
+//        self.notes = notes
+//        self.isDataGood = isDataGood
 //        self.info = info
 //        self.data = data
 //    }
 //    
 //}
 //
-//@Model
-//class WorkoutInfoData{
-//    let date: Date
-//    let username: String
-//    let movement: String
-//    let rounds: Int
-//    let reps: Int
-//    let weight: Double
+//class WorkoutInfo: EmbeddedObject {
 //    
-//    init(date: Date, username: String, movement: String, rounds: Int, reps: Int, weight: Double) {
-//        self.date = date
-//        self.username = username
+//    @Persisted var movement: String
+//    @Persisted var rounds: Int
+//    @Persisted var reps: Int
+//    @Persisted var weight: Double?
+//    
+//    convenience init( movement: String, rounds: Int, reps: Int, weight: Double? = nil) {
+//        self.init()
 //        self.movement = movement
 //        self.rounds = rounds
 //        self.reps = reps
@@ -127,14 +196,26 @@ class GyroscopeData: EmbeddedObject {
 //    }
 //}
 //
-//@Model
-//class AccelerometerSnapshot{
-//    var timestamp: TimeInterval
-//    var accelerationX: Double
-//    var accelerationY: Double
-//    var accelerationZ: Double
+//class WorkoutData: EmbeddedObject {
+//    @Persisted var accelerometerSnapshots = List <AccelerometerData>()
+//    @Persisted var gyroscopeSnapshots = List <GyroscopeData>()
+//    
+//    convenience init(accelerometerSnapshots: [AccelerometerData], gyroscopeSnapshots: [GyroscopeData]) {
+//        self.init()
+//        self.accelerometerSnapshots.append(objectsIn: accelerometerSnapshots)
+//        self.gyroscopeSnapshots.append(objectsIn: gyroscopeSnapshots)
+//    }
+//    
+//}
 //
-//    init(timestamp: TimeInterval, accelerationX: Double, accelerationY: Double, accelerationZ: Double) {
+//class AccelerometerData: EmbeddedObject {
+//    @Persisted var timestamp: TimeInterval
+//    @Persisted var accelerationX: Double
+//    @Persisted var accelerationY: Double
+//    @Persisted var accelerationZ: Double
+//
+//    convenience init(timestamp: TimeInterval, accelerationX: Double, accelerationY: Double, accelerationZ: Double) {
+//        self.init()
 //        self.timestamp = timestamp
 //        self.accelerationX = accelerationX
 //        self.accelerationY = accelerationY
@@ -142,28 +223,17 @@ class GyroscopeData: EmbeddedObject {
 //    }
 //}
 //
-//@Model
-//class GyroscopeSnapshot{
-//    var timestamp: TimeInterval
-//    var rotationX: Double
-//    var rotationY: Double
-//    var rotationZ: Double
+//class GyroscopeData: EmbeddedObject {
+//    @Persisted var timestamp: TimeInterval
+//    @Persisted var rotationX: Double
+//    @Persisted var rotationY: Double
+//    @Persisted var rotationZ: Double
 //
-//    init(timestamp: TimeInterval, rotationX: Double, rotationY: Double, rotationZ: Double) {
+//    convenience init(timestamp: TimeInterval, rotationX: Double, rotationY: Double, rotationZ: Double) {
+//        self.init()
 //        self.timestamp = timestamp
 //        self.rotationX = rotationX
 //        self.rotationY = rotationY
 //        self.rotationZ = rotationZ
-//    }
-//}
-//
-//@Model
-//class WorkoutMotionData {
-//    var accelerometerSnapshots: [AccelerometerSnapshot]
-//    var gyroscopeSnapshots: [GyroscopeSnapshot]
-//
-//    init(accelerometerSnapshots: [AccelerometerSnapshot], gyroscopeSnapshots: [GyroscopeSnapshot]) {
-//        self.accelerometerSnapshots = accelerometerSnapshots
-//        self.gyroscopeSnapshots = gyroscopeSnapshots
 //    }
 //}
